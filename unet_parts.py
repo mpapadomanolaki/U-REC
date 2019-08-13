bc7e89529dcc086dd6d4af8b71a117b7f0894b2b
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2019-08-13 13:17:44 +0300

    Create unet_parts.py

---
+++ unet_parts.py
@@ -0,0 +1,57 @@
+# sub-parts of the U-Net model
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class conv_block(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
+    def __init__(self, in_ch, out_ch):
+        super(conv_block, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class MaxP(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(MaxP, self).__init__()
+        self.mpool = nn.Sequential(
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        x = self.mpool(x)
+        return x
+
+
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
+
+        self.conv = conv_block(in_ch, out_ch)
+
+    def forward(self, x):
+        x2 = self.up(x)
+        return x2
+
+class outconv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outconv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
