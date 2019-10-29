7e534652ea6ea6d87ff55410a1a4c5d4d62979b2
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2019-10-29 12:34:24 -0400

    Update custom.py

--- custom.py
+++ custom.py
@@ -8,12 +8,6 @@ class MyDataset(Dataset):
         # Read the csv file and shuffle it
         self.data_info = pd.read_csv(csv_path, header=None)
         self.data_info = self.data_info.iloc[np.random.permutation(len(self.data_info))]
-        # First column contains the image paths
-        self.X = self.data_info.iloc[:,0]
-        # Second column is the labels
-        self.Y = self.data_info.iloc[:,1]
-        # Third column is the image ID
-        self.IM_id = self.data_info.iloc[:,2]
 
         ###create image/label list
         self.images = []
@@ -28,10 +22,11 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         # Get image patch
-        x = int(self.X[index])
-        y = int(self.Y[index])
-        image_id = int(self.IM_id[index]) - 7000
+        x = int(self.data_info.iloc[index,0])
+        y = int(self.data_info.iloc[index,1])
+        image_id = int(self.data_info.iloc[index,2])
 
+        print(x,y,image_id)
         image_patch = self.images[image_id] [x:x + self.patch_size, y:y + self.patch_size, :]
         image_patch = np.transpose(image_patch, (2,0,1))
         label_patch = self.labels[image_id] [x:x + self.patch_size, y:y + self.patch_size]
